049ef6091ba4e3573a201001e12a5be6b33a5a21
SLAE-1537 <64339469+SLAE-1537@users.noreply.github.com>
2020-04-30 07:15:12 +0300

    Update rc4.c

--- Assignment7/rc4.c
+++ Assignment7/rc4.c
@@ -1,5 +1,7 @@
 /*
-This code has been created for completing the requirements of the x86 Assembly Language and Shellcoding course.
+This code has been created for completing the requirements of the SecurityTube Linux Assembly Expert certification:
+  	
+http://securitytube-training.com/online-courses/securitytube-linux-assembly-expert/  
 
 Student ID: SLAE-1537
 */
